c8b455d760030861f9cfcfa08b58f0397117389f
Lj Albia <19635626+PinoyWH1Z@users.noreply.github.com>
2022-09-04 17:04:10 +0800

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace AoratosWin
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btn_Commence = new System.Windows.Forms.Button();
+            this.lbl_About = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Commence
+            // 
+            this.btn_Commence.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Commence.Location = new System.Drawing.Point(79, 55);
+            this.btn_Commence.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Commence.Name = "btn_Commence";
+            this.btn_Commence.Size = new System.Drawing.Size(109, 37);
+            this.btn_Commence.TabIndex = 0;
+            this.btn_Commence.Text = "Commence";
+            this.btn_Commence.UseVisualStyleBackColor = true;
+            this.btn_Commence.Click += new System.EventHandler(this.btn_Commence_Click);
+            // 
+            // lbl_About
+            // 
+            this.lbl_About.AutoSize = true;
+            this.lbl_About.Cursor = System.Windows.Forms.Cursors.Help;
+            this.lbl_About.Location = new System.Drawing.Point(216, 130);
+            this.lbl_About.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_About.Name = "lbl_About";
+            this.lbl_About.Size = new System.Drawing.Size(47, 13);
+            this.lbl_About.TabIndex = 1;
+            this.lbl_About.Text = "[ About ]";
+            this.lbl_About.Click += new System.EventHandler(this.lbl_About_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 154);
+            this.Controls.Add(this.lbl_About);
+            this.Controls.Add(this.btn_Commence);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(284, 193);
+            this.MinimumSize = new System.Drawing.Size(284, 193);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AoratosWin";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Commence;
+        private System.Windows.Forms.Label lbl_About;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,394 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace AoratosWin
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        int actedDirsCount = 0;
+        int actedFilesCount = 0;
+        int actedRegsCount = 0;
+        int actedDirsCount_recheck = 0;
+        int actedFilesCount_recheck = 0;
+        int actedRegsCount_recheck = 0;
+
+        private string getCurrentSID()
+        {
+            string MySIDs = "";
+
+            try
+            {
+                Process p = new Process();
+
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.Arguments = @"/c whoami /user";
+
+                p.Start();
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+
+                var rx = new Regex(@"S-1-[0-59]-\d{2}-\d{8,10}-\d{8,10}-\d{8,10}-[1-9]\d{3}");
+                var matches = rx.Matches(output);
+
+                foreach (Match match in matches)
+                {
+                    MySIDs = match.ToString();
+                }
+            }
+            catch {
+                
+            }
+
+            return MySIDs;
+        }
+
+        private void cleanTempPrefetchEtc(string DIR)
+        {
+            foreach (string subDirectory in Directory.GetDirectories(DIR))
+            {
+                try
+                {
+                    actedDirsCount += 1;
+                    Directory.Delete(subDirectory, true);
+                }
+                catch
+                {
+                }
+            }
+
+            foreach (string tempfile in Directory.GetFiles(DIR, "*.*", SearchOption.TopDirectoryOnly))
+            {
+                try
+                {
+                    actedFilesCount += 1;
+                    System.IO.File.Delete(tempfile);
+                }
+                catch
+                {
+                    //MessageBox.Show("error on " + tempfile);
+                }
+            }
+
+        }
+
+        private void cleanTempPrefetchEtc_recheck(string DIR)
+        {
+            foreach (string subDirectory in Directory.GetDirectories(DIR))
+            {
+                try
+                {
+                    actedDirsCount_recheck += 1;
+                }
+                catch
+                {
+                }
+            }
+
+            foreach (string tempfile in Directory.GetFiles(DIR, "*.*", SearchOption.TopDirectoryOnly))
+            {
+                try
+                {
+                    actedFilesCount_recheck += 1;
+                }
+                catch
+                {
+                }
+            }
+
+        }
+
+        private void removeRegTrace()
+        {
+
+            //REMOVE ROOT MUI CACHE
+            try
+            {
+                const string REGISTRY_ROOT = @"Local Settings\Software\Microsoft\Windows\Shell\MuiCache\";
+                using (RegistryKey rootKey = Registry.ClassesRoot.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount += 1;
+                            rootKey.DeleteValue(currSubKey);
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+
+            //REMOVE CURRENT USER MUI CACHE
+            try
+            {
+                const string REGISTRY_ROOT = @"SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache\";
+                using (RegistryKey rootKey = Registry.CurrentUser.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount += 1;
+                            rootKey.DeleteValue(currSubKey);
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+
+            //REMOVE USER MUI CACHE
+            try
+            {
+                string REGISTRY_ROOT = getCurrentSID() + "\\" + @"SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache\";
+                using (RegistryKey rootKey = Registry.Users.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount += 1;
+                            rootKey.DeleteValue(currSubKey);
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+
+            //REMOVE IN CURRENT USER COMPAT STORE
+            try
+            {
+                const string REGISTRY_ROOT = @"Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store\";
+                using (RegistryKey rootKey = Registry.CurrentUser.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount += 1;
+                            rootKey.DeleteValue(currSubKey);
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+
+            //REMOVE USER COMPAT STORE
+            try
+            {
+                string REGISTRY_ROOT = getCurrentSID() + "\\" + @"Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store\";
+                using (RegistryKey rootKey = Registry.Users.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount += 1;
+                            rootKey.DeleteValue(currSubKey);
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+        }
+        private void removeRegTrace_recheck()
+        {
+
+            //REMOVE ROOT MUI CACHE
+            try
+            {
+                const string REGISTRY_ROOT = @"Local Settings\Software\Microsoft\Windows\Shell\MuiCache\";
+                using (RegistryKey rootKey = Registry.ClassesRoot.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount_recheck += 1;
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+
+            //REMOVE CURRENT USER MUI CACHE
+            try
+            {
+                const string REGISTRY_ROOT = @"SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache\";
+                using (RegistryKey rootKey = Registry.CurrentUser.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount_recheck += 1;
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+
+            //REMOVE USER MUI CACHE
+            try
+            {
+                string REGISTRY_ROOT = getCurrentSID() + "\\" + @"SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache\";
+                using (RegistryKey rootKey = Registry.Users.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount_recheck += 1;
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+
+            //REMOVE IN CURRENT USER COMPAT STORE
+            try
+            {
+                const string REGISTRY_ROOT = @"Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store\";
+                using (RegistryKey rootKey = Registry.CurrentUser.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount_recheck += 1;
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+
+            //REMOVE USER COMPAT STORE
+            try
+            {
+                string REGISTRY_ROOT = getCurrentSID() + "\\" + @"Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store\";
+                using (RegistryKey rootKey = Registry.Users.OpenSubKey(REGISTRY_ROOT, true))
+                {
+                    if (rootKey != null)
+                    {
+                        string[] valueNames = rootKey.GetValueNames();
+                        foreach (string currSubKey in valueNames)
+                        {
+                            actedRegsCount_recheck += 1;
+                        }
+                        rootKey.Close();
+                    }
+
+                }
+            }
+            catch { }
+        }
+
+        public static bool IsAdministrator()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent()))
+                      .IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void btn_Commence_Click(object sender, EventArgs e)
+        {
+
+            //REMOVE TRACES IN REGISTRY
+            removeRegTrace();
+            removeRegTrace_recheck();
+
+            //REMOVE TRACES IN TEMP AND PREFETCH
+            string folderPath = "";
+
+            string[] targetDirs = {
+                "%systemdrive%\\Windows\\Temp",
+                "%userprofile%\\AppData\\Local\\Temp",
+                "%systemdrive%\\Windows\\prefetch"
+            };
+
+            foreach (string i in targetDirs)
+            {
+                try
+                {
+                    folderPath = Environment.ExpandEnvironmentVariables(i);
+                    cleanTempPrefetchEtc(folderPath);
+                }
+                catch { }
+            }
+
+            foreach (string i in targetDirs)
+            {
+                try
+                {
+                    folderPath = Environment.ExpandEnvironmentVariables(i);
+                    cleanTempPrefetchEtc_recheck(folderPath);
+                }
+                catch { }
+            }
+
+            double total_acted = actedDirsCount + actedFilesCount + actedRegsCount;
+            double total_recheck = actedDirsCount_recheck + actedFilesCount_recheck + actedRegsCount_recheck;
+            double covered = (total_recheck / total_acted) * 100;
+            string strCovered = covered.ToString("0.00");
+
+            MessageBox.Show("Concluded. [ Removed " + strCovered + "% ]", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //reset
+            actedDirsCount = 0;
+            actedFilesCount = 0;
+            actedRegsCount = 0;
+            actedDirsCount_recheck = 0;
+            actedFilesCount_recheck = 0;
+            actedRegsCount_recheck = 0;
+        }
+
+        private void lbl_About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("AoratosWin is a tool that removes traces of executed applications on Windows OS \nwhich can easily be listed with tools such as ExecutedProgramList by Nirsoft.", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            if (!IsAdministrator())
+                MessageBox.Show("You did not run this tool as Admin, which means the removal of some system-level logs is unfeasible.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        }
+    }
+}
